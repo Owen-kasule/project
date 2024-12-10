b24d2fc523a7d8425735b84c0da6f3fda40ef5c5
Owen-kasule <owenatug@gmail.com>
2024-12-10 17:53:20 +0300

    refactor styles and scripts for improved readability, maintainability, and visual consistency

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Display the last modified date
+    // Display the last modified date in the footer
     const lastModifiedSpan = document.getElementById('last-modified');
     const lastModified = document.lastModified;
     lastModifiedSpan.textContent = `Last Modified: ${lastModified}`;
